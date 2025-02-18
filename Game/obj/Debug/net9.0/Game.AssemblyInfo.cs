dea50ef9dcbc93c1f8f74efc435ec97299523928
ich <ich@localhost.localdomain>
2025-02-18 12:51:45 -0500

    Fix

--- Game/obj/Debug/net9.0/Game.AssemblyInfo.cs
+++ Game/obj/Debug/net9.0/Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b17cf4a6ca6e8836ade2e934e27fd5ea823d8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25150fb7b0cf9438691587d82d1d4f2a3b5f6111")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
